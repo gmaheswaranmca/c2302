8b6eec7834c2be9bb3dc134e0acd9487cd52cb74
Maheswaran <pystud19@gmail.com>
2022-07-27 17:03:20 +0530

    NOTES

--- Days/Day 12 - 202207J27W/Update-to-change-for-multiple-rows-different-data.sql
+++ Days/Day 12 - 202207J27W/Update-to-change-for-multiple-rows-different-data.sql
@@ -3,6 +3,7 @@ CREATE TABLE emp(
 	full_name varchar(100),
 	salary float, 	
 	dept_id int);
+	
 INSERT INTO emp	
 VALUES(10,'rahul',NULL,	10),
 (20,'stutee',NULL,	20),
@@ -11,6 +12,7 @@ VALUES(10,'rahul',NULL,	10),
 CREATE TABLE emp_sal(
 	emp_id int, 
 	salary float);
+	
 INSERT INTO emp_sal
 VALUES(10,7000),
 		(20,8000),
@@ -24,22 +26,31 @@ SET emp.salary = sal.salary
 FROM emp_sal sal 
 WHERE (emp.emp_id=sal.emp_id);
 
+UPDATE emp 
+SET emp.salary = emp_sal.salary 
+FROM emp_sal 
+WHERE (emp.emp_id=emp_sal.emp_id);
+
+/*
+emp_management.quest_employee_basic emp
+emp_management.quest_employee_salary sal 
+*/
 
 DROP TABLE emp;
 
 
-	Update emp 
-	SET salary = CASE 
-		WHEN emp_id=10 THEN 5000 
-		WHEN emp_id=20 THEN 7000 
-		WHEN emp_id=30 THEN 8000
-		ELSE salary 
-		END;
-	
-	Update emp 
-	SET salary = CASE emp_id
-		WHEN 10 THEN 5000 
-		WHEN 20 THEN 7000 
-		WHEN 30 THEN 8000
-		ELSE salary 
-		END;
+Update emp 
+SET salary = CASE 
+	WHEN emp_id=10 THEN 5000 
+	WHEN emp_id=20 THEN 7000 
+	WHEN emp_id=30 THEN 8000
+	ELSE salary 
+	END;
+
+Update emp 
+SET salary = CASE emp_id
+	WHEN 10 THEN 5000 
+	WHEN 20 THEN 7000 
+	WHEN 30 THEN 8000
+	ELSE salary 
+	END;
