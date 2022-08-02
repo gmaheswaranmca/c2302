b78579d952a1abf3f2b2c58ff32f220bf5d41477
Maheswaran <pystud19@gmail.com>
2022-08-02 15:31:37 +0530

    NOTES

---
+++ Days/Day 18 - 202208A02T/Person-Class/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsLearningApp
+{
+    class Person
+    {
+        private string name;
+        private string place;
+        private string phoneNumber;
+        private int age;
+        private string dateOfBirth;
+
+        public void Read()
+        {
+            Console.WriteLine("Enter Name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Place:");
+            place = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number:");
+            phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Age:");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Date Of Birth:");
+            dateOfBirth = Console.ReadLine();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name:{name}");
+            Console.WriteLine($"Place:{place}");
+            Console.WriteLine($"Phone Number:{phoneNumber}");
+            Console.WriteLine($"Age:{age}");
+            Console.WriteLine($"Date Of Birth:{dateOfBirth}");
+        }
+    }
+}
---
+++ Days/Day 18 - 202208A02T/Person-Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsLearningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person maheswaran = new Person();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Enter personal details of maheswaran:");
+            Console.WriteLine("-----------------------------------------");
+            maheswaran.Read();
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Maheswaran profile is");
+            Console.WriteLine("-----------------------------------------");
+            maheswaran.Print();
+
+            Person gokulraj = new Person();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Enter personal details of gokulraj:");
+            Console.WriteLine("-----------------------------------------");
+            gokulraj.Read();
+
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("gokulraj profile is");
+            Console.WriteLine("-----------------------------------------");
+            gokulraj.Print();
+
+            Console.Read();
+        }
+    }
+}
