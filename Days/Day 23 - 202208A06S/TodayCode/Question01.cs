ddbde2d35095ed265c1fc8b3cdcd4081461edaef
Maheswaran <pystud19@gmail.com>
2022-08-06 15:17:26 +0530

    NOTES

---
+++ Days/Day 23 - 202208A06S/TodayCode/Question01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDay06082022
+{
+    class Question01
+    {
+        static void Main(string[] args)
+        {
+            Student firstStudent = new Student();
+            Student secondStudent = new Student();
+            //Input
+            Console.WriteLine("Enter 2 students student name, mark 1, mark 2, mark 3 in each line:");
+            firstStudent.Read();
+            secondStudent.Read();
+            //Processing ie Calcultion
+            firstStudent.Calculate();
+            secondStudent.Calculate();
+            //Ouput
+            Console.WriteLine("Mark List");
+            Console.WriteLine("Student#1");
+            firstStudent.Display();
+            Console.WriteLine("Student#2");
+            secondStudent.Display();
+            Student.DisplayRollNumber();
+            Console.Read();
+        }
+    }
+}
---
+++ Days/Day 23 - 202208A06S/TodayCode/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDay06082022
+{
+    class Student
+    {
+        private int rollNumber;
+        private string studentName;
+        private int mark1;
+        private int mark2;
+        private int mark3;
+        private int total;
+        private float average;
+
+        private static int nextRollNumber;
+
+        public Student()
+        {
+            this.rollNumber = Student.nextRollNumber;
+            Student.nextRollNumber++;
+
+            this.studentName = "";
+            this.mark1 = 0;
+            this.mark2 = 0;
+            this.mark3 = 0;
+            this.total = 0;
+            this.average = 0.0f;
+        }
+        public void Read()
+        {
+            this.studentName = Console.ReadLine();
+            this.mark1 = int.Parse(Console.ReadLine());
+            this.mark2 = int.Parse(Console.ReadLine());
+            this.mark3 = int.Parse(Console.ReadLine());
+        }
+        public void Calculate()
+        {
+            this.total = this.mark1 + this.mark2 + this.mark3;
+            this.average = ((float)this.total / 3.0f);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {this.studentName}");
+            Console.WriteLine($"Roll Number: {this.rollNumber}");
+            Console.WriteLine($"Mark 1: {this.mark1}");
+            Console.WriteLine($"Mark 2: {this.mark2}");
+            Console.WriteLine($"Mark 3: {this.mark3}");
+            Console.WriteLine($"Total: {this.total}");
+            Console.WriteLine($"Average: {this.average}");
+        }
+        public static void DisplayRollNumber()
+        {
+            Console.WriteLine($"Next Roll Number is {Student.nextRollNumber}");
+        }
+        static Student()
+        {
+            Student.nextRollNumber = 1001;
+        }
+    }
+}
