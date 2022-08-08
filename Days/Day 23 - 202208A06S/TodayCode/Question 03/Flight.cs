de95480a1e1df21b317f8d5d2a24d150b96270f6
Maheswaran <pystud19@gmail.com>
2022-08-08 17:54:47 +0530

    NOTES

---
+++ Days/Day 23 - 202208A06S/TodayCode/Question 03/Flight.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDay06082022
+{
+    class Flight
+    {
+        private int fightNumber;
+        private string sourcePoint;
+        private string destinationPoint;
+        private int noOfSeats;
+        private float ticketFare;
+        private int noOfBookedSeats;
+        private int noOfAvailableSeats;
+        private float totalBookingAmount;
+
+        private static int nextflightnumber;
+
+
+        public Flight(): this("","",0,0)
+        {
+            
+        }
+        public Flight(string p_sourcePoint, string p_destinationPoint,int p_noOfSeats, float p_ticketFare)
+        {
+            this.fightNumber = Flight.nextflightnumber;
+            Flight.nextflightnumber++;
+
+            this.sourcePoint = p_sourcePoint;
+            this.destinationPoint = p_destinationPoint;
+            this.noOfSeats = p_noOfSeats;
+            this.ticketFare = p_ticketFare;
+            this.noOfBookedSeats = 0;
+            this.noOfAvailableSeats = 0;
+            this.totalBookingAmount = 0;
+        }
+        public Flight(Flight anotherFlight) : this(anotherFlight.sourcePoint, anotherFlight.destinationPoint, anotherFlight.noOfSeats, anotherFlight.ticketFare)
+        {
+
+        }
+        public void Read()
+        //flightNumber, noOfAvailableSeats and totalBookingAmount
+        {
+            this.sourcePoint = Console.ReadLine();
+            this.destinationPoint = Console.ReadLine();
+            this.noOfSeats = int.Parse(Console.ReadLine());
+            this.ticketFare = (float)int.Parse(Console.ReadLine());
+
+        }
+        public void Calculate()
+        {
+            this.noOfAvailableSeats = this.noOfSeats - this.noOfBookedSeats;
+            this.totalBookingAmount = this.ticketFare * this.noOfBookedSeats;
+        }
+        public void BookATicket()
+        {
+            this.noOfBookedSeats++;
+        }
+        public void CancelATicket()
+        {
+            this.noOfBookedSeats--;
+
+        }
+        public void display()
+        {
+            Console.WriteLine($"Flight No: {this.fightNumber}");
+            Console.WriteLine($"Source Point: {this.sourcePoint}");
+            Console.WriteLine($"Destination Point: {this.destinationPoint}");
+            Console.WriteLine($"No of Seats: {this.noOfSeats}");
+            Console.WriteLine($"No of Booked Seats: {this.noOfBookedSeats}");
+            Console.WriteLine($"Ticket Fare: {this.ticketFare}");
+            Console.WriteLine($"Total Booking Amount: {this.totalBookingAmount}");
+            Console.WriteLine($"No of Available Seats: {this.noOfAvailableSeats}");
+
+        }
+        public double GetTotalBookingAmount()
+        {
+
+            return this.totalBookingAmount;
+
+        }
+
+
+        public void DisplayAfterRead()
+        {
+            Console.WriteLine("(" + this.fightNumber + ")" + this.sourcePoint + "-" + this.destinationPoint);
+
+        }
+        public static void Displaynextflightnumber()
+        {
+
+            Console.WriteLine($"Next Flight Number is {Flight.nextflightnumber}");
+        }
+
+        static Flight()
+        {
+            nextflightnumber = 1011;
+        }
+    }
+}
---
+++ Days/Day 23 - 202208A06S/TodayCode/Question 03/Question03.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDay06082022
+{
+    class Question03
+    {
+        static void Main()
+        {
+            Console.WriteLine("We experiment the flight object ");
+            Flight flight1 = new Flight();
+            Flight flight2 = new Flight("","",0,0);
+            Flight flight3 = new Flight(flight2);
+
+            bool checker = true;
+            
+            Console.WriteLine("Enter 3 flight details source, destination, no of seats, ticket fare in each line:");
+            flight1.Read();
+            flight2.Read();
+            flight3.Read();
+
+            do
+            {
+                int optionTranType = 0, choiceForFlight = 0;
+
+
+                Console.Write("Choice(1-Booking,2-Cancellation):");
+                optionTranType = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Pick the flight");
+                Console.Write("1."); flight1.DisplayAfterRead();
+                Console.Write("2."); flight2.DisplayAfterRead();
+                Console.Write("3."); flight3.DisplayAfterRead();
+                Console.Write("Your choice(use the flight serial number): ");
+                choiceForFlight = Convert.ToInt32(Console.ReadLine());
+                
+                if (optionTranType == 1)//---------------------For Booking-----------------------------------
+                {
+                    if (choiceForFlight == 1)//flight 1
+                    {
+                        flight1.BookATicket();
+                    }
+                    else if (choiceForFlight == 2)//flight 2
+                    {
+                        flight2.BookATicket();
+                    }
+                    else if (choiceForFlight == 3)//flight 3
+                    {
+                        flight3.BookATicket();
+                    }
+                }
+                else if (optionTranType == 2)//-------------------------For Cancel---------------------------------
+                {
+                    if (choiceForFlight == 1)//flight 1
+                    {
+                        flight1.CancelATicket();
+                    }
+                    else if (choiceForFlight == 2)//flight 2
+                    {
+                        flight2.CancelATicket();
+                    }
+                    else if (choiceForFlight == 3)//flight 3
+                    {
+                        flight2.CancelATicket();
+                    }
+                }
+                Console.WriteLine("Are you sure to continue (true/false)");
+                checker = Convert.ToBoolean(Console.ReadLine());
+            } while (checker);
+
+            double turnOver = 0;
+
+            flight1.Calculate();
+            turnOver += flight1.GetTotalBookingAmount();
+
+            flight2.Calculate();
+            turnOver += flight2.GetTotalBookingAmount();
+            
+            flight3.Calculate();
+            turnOver += flight3.GetTotalBookingAmount();
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Flight #1");
+            flight1.display();
+            
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Flight #2");
+            flight2.display();
+            
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Flight #3");
+            flight3.display();
+            
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"Total TurnOver:{turnOver}");
+            Console.WriteLine("----------------------------------------------------");
+
+
+            Flight.Displaynextflightnumber();
+            Console.Read();
+        }
+    }
+}
