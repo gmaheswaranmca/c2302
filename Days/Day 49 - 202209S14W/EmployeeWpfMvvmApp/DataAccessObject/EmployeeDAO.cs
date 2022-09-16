210f5916a31c0daff00b12ab9967b6f9d2059b4e
Maheswaran <pystud19@gmail.com>
2022-09-16 17:29:38 +0530

    NOTES

---
+++ Days/Day 49 - 202209S14W/EmployeeWpfMvvmApp/DataAccessObject/EmployeeDAO.cs
@@ -0,0 +1,88 @@
+﻿using EmployeeWpfMvvmApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWpfMvvmApp.DataAccessObject
+{
+    static public class EmployeeDAO
+    {
+        public static void Create(EmployeeModel employee)
+        {
+            string insertString = @"INSERT INTO employee
+(employee_id,employee_name,job_title,salary) 
+VALUES(@employee_id,@employee_name,@job_title,@salary);";
+
+            SqlConnection con = new SqlConnection(DbConfig.ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(insertString, con);
+            cmd.Parameters.AddWithValue("@employee_id", employee.Id);
+            cmd.Parameters.AddWithValue("@employee_name", employee.Name);
+            cmd.Parameters.AddWithValue("@job_title", employee.JobTitle);
+            cmd.Parameters.AddWithValue("@salary", employee.Salary);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void Update(EmployeeModel employee)
+        {
+            string insertString = @"UPDATE employee SET 
+employee_name=@employee_name,
+job_title=@job_title,
+salary=@salary 
+WHERE employee_id=@employee_id;";
+
+            SqlConnection con = new SqlConnection(DbConfig.ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(insertString, con);
+            cmd.Parameters.AddWithValue("@employee_id", employee.Id);
+            cmd.Parameters.AddWithValue("@employee_name", employee.Name);
+            cmd.Parameters.AddWithValue("@job_title", employee.JobTitle);
+            cmd.Parameters.AddWithValue("@salary", employee.Salary);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public static void Delete(int employee_id)
+        {
+            string insertString = @"DELETE FROM employee 
+WHERE employee_id=@employee_id;";
+
+            SqlConnection con = new SqlConnection(DbConfig.ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(insertString, con);
+            cmd.Parameters.AddWithValue("@employee_id", employee_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static ObservableCollection<EmployeeModel> ReadAll()
+        {
+            ObservableCollection<EmployeeModel> employees = new ObservableCollection<EmployeeModel>();
+
+            string insertString = @"SELECT employee_id,
+employee_name,job_title,salary 
+FROM employee;";
+
+            SqlConnection con = new SqlConnection(DbConfig.ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(insertString, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                employees.Add(new EmployeeModel(
+                    (int)reader["employee_id"],
+                    (string)reader["employee_name"],
+                    (string)reader["job_title"],
+                    (double)reader["salary"]
+                    ));
+            }
+            con.Close();
+
+            return employees;
+        }
+    }
+}
---
+++ Days/Day 49 - 202209S14W/EmployeeWpfMvvmApp/SqlScript.sql
@@ -0,0 +1,67 @@
+﻿CREATE TABLE employee
+(employee_id INT, 
+employee_name VARCHAR(256) NOT NULL DEFAULT '', 
+job_title VARCHAR(50) NOT NULL DEFAULT '', 
+salary float NOT NULL DEFAULT 0,
+PRIMARY KEY(employee_id)
+);
+GO 
+
+INSERT INTO [dbo].[employee] ([employee_id], [employee_name], [job_title], [salary]) 
+VALUES (1001, N'Maheswaran', N'Trainer', 1001)
+INSERT INTO [dbo].[employee] ([employee_id], [employee_name], [job_title], [salary]) 
+VALUES (1002, N'Ashish Ranjan', N'Programmer Analyst', 7500)
+INSERT INTO [dbo].[employee] ([employee_id], [employee_name], [job_title], [salary]) 
+VALUES (1003, N'Logesh Rajan', N'Software Engineer', 8000)
+INSERT INTO [dbo].[employee] ([employee_id], [employee_name], [job_title], [salary]) 
+VALUES (1004, N'Gokul', N'Software Engineer', 5500)
+INSERT INTO [dbo].[employee] ([employee_id], [employee_name], [job_title], [salary]) 
+VALUES (1005, N'Stutee [SP]', N'Programmer', 7800)
+GO
+
+CREATE PROCEDURE employee_add
+	@employee_id int,
+	@employee_name varchar(256),
+	@job_title  varchar(256),
+	@salary float
+AS
+BEGIN
+	INSERT INTO employee
+	(employee_id,employee_name,job_title,salary) 
+	VALUES(@employee_id,@employee_name,@job_title,@salary);
+END
+GO 
+
+CREATE PROCEDURE employee_update
+	@employee_id int,
+	@employee_name varchar(256),
+	@job_title  varchar(256),
+	@salary float
+AS
+BEGIN
+	UPDATE employee 
+	SET 
+		employee_name=@employee_name,
+		job_title=@job_title,
+		salary=@salary 
+	WHERE employee_id=@employee_id;
+END
+GO 
+
+CREATE PROCEDURE employee_delete
+	@employee_id int
+AS
+BEGIN
+	DELETE FROM employee 
+	WHERE employee_id=@employee_id;
+END
+GO 
+
+CREATE PROCEDURE employee_select	
+AS
+BEGIN
+	SELECT employee_id,
+		employee_name,job_title,salary 
+	FROM employee;
+END
+GO 
